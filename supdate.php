1628d0c761886eb5a06a614f71b63b311a71c743
cdp181 <chris@smokoingcures.com>
2015-08-28 01:53:51 +0100

    Update supdate.php

--- supdate.php
+++ supdate.php
@@ -7,8 +7,9 @@ $server_id = $_GET["server_id"];
 $server_name = $_GET["server_name"];
 $wol = $_GET["wol"];
 $server_mac = $_GET["server_mac"];
+$section_size = $_GET["section_size"];
 
-$db->exec("UPDATE servers SET server_name='$server_name',wol='$wol',server_mac='$server_mac' WHERE server_id='$server_id'");
+$db->exec("UPDATE servers SET server_name='$server_name',wol='$wol',server_mac='$server_mac',section_size='$section_size' WHERE server_id='$server_id'");
 
 ?>
 Updated!
