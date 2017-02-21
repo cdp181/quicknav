da9c849ba3e9b15e2e3da202dc9a125ca5d1db32
cdp181 <chris@smokingcures.com>
2017-02-21 17:18:47 +0000

    Add descriptions

--- apache.sh
+++ apache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# Create blanks Database if doesn't exist in /data
 if [ ! -f /data/quicknav.db ]; then cat /tmp/database.txt | sqlite3 /data/quicknav.db; fi
 
+# Apache stuff
 exec /usr/sbin/apache2 -D FOREGROUND
