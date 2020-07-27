1f5977df60626f9bb9d0af800e61eafb3aa8bb18
Chris Pollard <chris@smokingcures.com>
2020-07-27 16:05:07 +0100

    Update darrhax.php

--- darrhax.php
+++ darrhax.php
@@ -48,9 +48,9 @@ tr.app {background-color:#ff9900;margin:0;border:0;padding:0;}
 <table style="width:95%" class="collaptable">
 <?php
 
-$db = new SQLite3('/data/quicknav.db') or die('DB Open failed');
+$db = new SQLite3('/data/darrhax.db') or die('DB Open failed');
 
-$result = $db->query('SELECT server_id, server_name, server_mac, wol, section_size, server_ip FROM servers');
+$result = $db->query('SELECT db_id, db_name, db_path FROM dbs');
 
 $tableid = 1;
 $tableidapp = 20;
@@ -58,54 +58,18 @@ $tableidapp = 20;
 while ($row = $result->fetchArray())
  {
    ?><tr class="server" data-id="<?php echo $tableid;?>" data-parent=""><form action="supdate.php" method="get" class="ajaxform"><input type="hidden" name="server_id" value="<?php echo $row['server_id'];?>">
-      <td>Name : <input type="text" name="server_name" value="<?php echo "{$row['server_name']}";?>"></td>
-      <td>Section Size : <select name="section_size">
-	    <option value="12" <?php if ($row['section_size']==12) {?>selected="selected"<?php };?>>Full row</option>
-	    <option value="6" <?php if ($row['section_size']==6) {?>selected="selected"<?php };?>>Half row</option>
-	    <option value="4" <?php if ($row['section_size']==4) {?>selected="selected"<?php };?>>Small</option>
-	  </td>
-	  <td>WOL : <select name="wol">
-	    <option value="1" <?php if ($row['wol']==1) {?>selected="selected"<?php };?>>Yes</option>
-	    <option value="0" <?php if ($row['wol']==0) {?>selected="selected"<?php };?>>No</option>
-	  </td>
-	  <td>Mac : <input type="text" name="server_mac" size="20" value="<?php echo $row['server_mac'];?>" maxlength="17" size="17"></td>
-	  <td><input type="submit" value="Update" title="Modify Server"></form></td><form action="sdelete.php" method="get" class="ajaxform"><td><input type="hidden" name="server_id" value="<?php echo $row['server_id'];?>"><input type="submit" value="Delete" title="Delete Server WARNING! Deletes all corresponding Apps"></form></td>
+      <td>Name : <input type="text" name="db_name" value="<?php echo "{$row['db_name']}";?>"></td>
+	  <td>Path : <input type="text" name="db_path" size="20" value="<?php echo $row['db_path'];?>" ></td>
+	  <td><input type="submit" value="Update" title="Modify Server"></form></td><form action="dbdelete.php" method="get" class="ajaxform"><td><input type="hidden" name="db_id" value="<?php echo $row['db_id'];?>"><input type="submit" value="Delete" title="Delete Server WARNING! Deletes all corresponding Apps"></form></td>
 	 </tr><?php
-   $result2 = $db->query('SELECT app_id, app_name, app_ip, app_port, app_url, server_id FROM apps WHERE server_id = ' . $row['server_id'] . '');
-     while ($row2 = $result2->fetchArray())
-	  {
-	    ?><tr class="app" data-id="<?php echo $tableidapp++;?>" data-parent="<?php echo $tableid;?>"><form action="aupdate.php" method="get" class="ajaxform"><input type="hidden" name="app_id" value="<?php echo $row2['app_id'];?>">
-		   <td>App Name : <input type="text" name="app_name" value="<?php echo "{$row2['app_name']}";?>"></td>
-		   <td>App IP : <input type="text" name="app_ip" value="<?php echo "{$row2['app_ip']}";?>" maxlength="15" size="15"></td>
-		   <td>App Port : <input type="text" name="app_port" value="<?php echo "{$row2['app_port']}";?>" maxlength="5" size="5"></td>
-		   <td>App URL : <input type="text" name="app_url" value="<?php echo "{$row2['app_url']}";?>" maxlength="128" size="50"></td>
-		   <td><input type="submit" value="Update" title="Modify App"></form></td><form action="adelete.php" method="get" class="ajaxform"><td><input type="hidden" name="app_id" value="<?php echo $row2['app_id'];?>"><input type="submit" value="Delete" title="Delete App"></form></td>
-		  </tr><?php
-	  }?>
-	<tr class="app" data-id="<?php echo $tableidapp++;?>" data-parent="<?php echo $tableid;?>"><form action="ainsert.php" method="get" class="ajaxform"><input type="hidden" name="server_id" value="<?php echo $row['server_id'];?>">
-		   <td>App Name : <input type="text" name="app_name" title="Application Name"></td>
-		   <td>App IP : <input type="text" name="app_ip" title="IP Address to test App" maxlength="15" size="15"></td>
-		   <td>App Port : <input type="text" name="app_port" title="Port to test App" maxlength="5" size="5"></td>
-		   <td>App URL : <input type="text" name="app_url" title="URL for App, Optional" maxlength="128" size="50"></td>
-		   <td colspan="2"><input type="submit" value="Insert" title="Add new App"></form></td>
-		   </tr><?php
         $tableid++;
    }  
 ?>
-<tr><form action="sinsert.php" method="get" class="ajaxform">
+<tr><form action="dbinsert.php" method="get" class="ajaxform">
 	  <td colspan="6">Add new server</td></tr>
-      <tr class="server"><td>Name : <input type="text" name="server_name" title="Server Name"></td>
-      <td>Section Size : <select name="section_size" title="Full row 4+ Apps, Half row 2-3 Apps, Small 1 App">
-	    <option value="12">Full row</option>
-	    <option value="6">Half row</option>
-	    <option value="4">Small</option>
-	  </td>
-	  <td>WOL : <select name="wol" title="Enable Wake-on-LAN Button">
-	    <option value="1">Yes</option>
-	    <option value="0">No</option>
-	  </td>
-	  <td>Mac : <input type="text" name="server_mac" size="20" title="MAC Address in format 12:34:45:ab:cd:56, Required for WoL" maxlength="17" size="17"></td>
-	  <td colspan="2"><input type="submit" value="Insert" title="Add new Server"></form></td>
+      <tr class="server"><td>Name : <input type="text" name="db_name" title="DB Name"></td>
+	  <td>Path : <input type="text" name="db_path" size="20" title="DB Path in docker"></td>
+	  <td colspan="2"><input type="submit" value="Insert" title="Add new DB"></form></td>
 	 </tr>
 
 </table>
