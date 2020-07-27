be64a943103c9ec92fe708cf426cab0dd06fb703
Chris Pollard <chris@smokingcures.com>
2020-07-27 16:38:43 +0100

    Update and rename apache.sh to dbcreate.sh

--- dbcreate.sh
+++ dbcreate.sh
@@ -2,6 +2,3 @@
 # Create blanks Database if doesn't exist in /data
 if [ ! -f /data/quicknav.db ]; then cat /tmp/database.txt | sqlite3 /data/quicknav.db; fi
 if [ ! -f /data/darrhax.db ]; then cat /tmp/darrhax.txt | sqlite3 /data/darrhax.db; fi
-
-# Apache stuff
-exec /usr/sbin/apache2 -D FOREGROUND
