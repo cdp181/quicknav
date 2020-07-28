a0a7e974870572ba325c8aae88a40dca4692f603
Chris Pollard <chris@smokingcures.com>
2020-07-28 17:17:04 +0100

    Delete dbcreate.sh

--- dbcreate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Create blanks Database if doesn't exist in /data
-if [ ! -f /data/quicknav.db ]; then cat /tmp/database.txt | sqlite3 /data/quicknav.db; fi
-if [ ! -f /data/darrhax.db ]; then cat /tmp/darrhax.txt | sqlite3 /data/darrhax.db; fi
